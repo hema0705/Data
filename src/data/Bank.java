48ea81e35fa5cf046d82109110f4dbcd7e17212e
hema0705 <informtohema7@gmail.com>
2021-07-22 12:29:50 +0530

    task

---
+++ src/data/Bank.java
@@ -0,0 +1,19 @@
+package data;
+
+public class Bank {
+public static void main(String[] args) {
+	System.out.println("Enter the double value");
+	double d=657.76;
+	System.out.println("double value is " +d);
+	System.out.println(" Enter the string value");
+	String s="Hi hello";
+	System.out.println("string value is " +s);
+	
+	System.out.println("Enter the long value");
+	long l=76435799l;
+	System.out.println("long value is " +l);
+	System.out.println("Enter the char value");
+	char c='H';
+	System.out.println("Char value is " +c);
+}
+}
